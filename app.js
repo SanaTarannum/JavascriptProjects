389ddacd8d13f9c9a470acb230bddba4bdbc12e4
Sana Tarannum <156060363+SanaTarannum@users.noreply.github.com>
2025-03-10 12:17:23 +0530

    Add operators app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ document.getElementById("btn").addEventListener("click", function () {
   let num1 = document.querySelector(".num1").value;
   let num2 = document.querySelector(".num2").value;
   let result = document.querySelector(".result");
-  let operator = document.getElementById("selectOp").value;
+  let operators = document.getElementById("selectOp").value;
 
-  switch (operator) {
+  switch (operators) {
     case "plus":
       result.innerHTML = Number(num1) + Number(num2);
       break;
